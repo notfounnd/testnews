7987eab347991b870da409b2b9603b2d32536593
Júnior Sbrissa <57534340+notfounnd@users.noreply.github.com>
2024-12-31 20:01:59 +0000

    feat: add `database.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
-function status(request, response) {
+import database from '../../../../infra/database';
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 AS SUM;');
+  console.log(result.rows);
   return response.status(200).json({ message: 'Hello API!' });
 }
 
