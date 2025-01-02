715686b698441595a945e0d66c75e66d15b7f482
Júnior Sbrissa <57534340+notfounnd@users.noreply.github.com>
2025-01-02 16:00:28 +0000

    build: add `jsconfiig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import database from '../../../../infra/database';
+import database from 'infra/database';
 
 async function status(request, response) {
   const result = await database.query('SELECT 1 + 1 AS SUM;');
